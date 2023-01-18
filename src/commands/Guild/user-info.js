3aebbf181b30d31bea0ea1949cb684a289e5e7fc
csj90 <admin@csjgaming.com>
2023-01-18 18:49:20 -0500

    updated whois
    had old code

--- src/commands/Guild/user-info.js
+++ src/commands/Guild/user-info.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 // Dependencies
 const { Embed } = require('../../utils'),
 	moment = require('moment'),
 	Command = require('../../structures/Command.js');
+
 
 /**
  * User-info command
@@ -43,12 +45,12 @@ class UserInfo extends Command {
 		// Get user
 		const members = await message.getMember();
 		const embed = this.createEmbed(bot, message.guild, members[0]);
-		
 
 		// send user info
 		message.channel.send({ embeds: [embed] });
 	}
-	
+
+
 	/**
  	 * Function for recieving interaction.
  	 * @param {bot} bot The instantiating client
@@ -59,38 +61,39 @@ class UserInfo extends Command {
 	*/
 	async callback(bot, interaction, guild, args) {
 		const member = guild.members.cache.get(args.get('user')?.value ?? interaction.user.id);
-		
+
+
 		// send embed
 		const embed = await this.createEmbed(bot, guild, member);
-		const getAcks = (ID) => {
-    let teamAcks = [];
-    const owners = ["105419972309741568","292532134877462529"];
-	const DiscordManager = ["882219448373772319","504510509106724867"];
-	const CommunityManager = ["189113141836120064"];
-	const Administrators = ["215853754828718080","114884883742064644", "170967044391567361","142824509613932544","105419972309741568"];
-	const Moderators = ["396062087907901441","258098010209255424","154099133949411328","275766959478407168","188008446921015307","148141365853814785","232917170483363840"];
-	const LegacyStaff = ["89695287366414336","129172043987025921","106048100002439168","169896575336710144"];
-	const Contributors = ["263895063573037057"];
-	const Developers = ["105419972309741568"];
-	const Friends = ["489133814765846538"];
-    // rest of your arrays goes here
- 
-    if (owners.includes(id)) teamAcks.push("Owners");
-	if (DiscordManager.includes(id)) teamAcks.push("Discord Managers");
-	if (CommunityManager.includes(id)) teamAcks.push("Community Managers");
-	if (Administrators.includes(id)) teamAcks.push("Administrators");
-	if (Moderators.includes(id)) teamAcks.push("Moderators");
-	if (LegacyStaff.includes(id)) teamAcks.push("Legacy Staff");
-	if (Contributors.includes(id)) teamAcks.push("Contributors");
-	if (Developers.includes(id)) teamAcks.push("Developers");
-	if (Friends.includes(id)) teamAcks.push("Friends");
-    // rest of your if statements go here
- 
-       return teamAcks;
-	};
-		//if(getAcks(ID).length >= 0)
-		//interaction.reply({ embeds: [embed] });
+
+
+		interaction.reply({ embeds: [embed] });
 	}
+
+	const acknowledgments = getAcks(ids) ; 
+		let teamAcks = [];
+		const owners = ['105419972309741568', '292532134877462529'];
+		const DiscordManager = ['882219448373772319', '504510509106724867'];
+		const CommunityManager = ['189113141836120064'];
+		const Administrators = ['215853754828718080', '114884883742064644', '170967044391567361', '142824509613932544', '105419972309741568'];
+		const Moderators = ['396062087907901441', '258098010209255424', '154099133949411328', '275766959478407168', '188008446921015307', '148141365853814785', '232917170483363840'];
+		const LegacyStaff = ['89695287366414336', '129172043987025921', '106048100002439168', '169896575336710144', '324646179134636043'];
+		const Contributors = ['263895063573037057'];
+		const Developers = ['105419972309741568'];
+		// rest of your arrays goes here
+
+		if (owners.includes(id)) teamAcks.push('Owners');
+		if (DiscordManager.includes(id)) teamAcks.push('Discord Managers');
+		if (CommunityManager.includes(id)) teamAcks.push('Community Managers');
+		if (Administrators.includes(id)) teamAcks.push('Administrators');
+		if (Moderators.includes(id)) teamAcks.push('Moderators');
+		if (LegacyStaff.includes(id)) teamAcks.push('Legacy Staff');
+		if (Contributors.includes(id)) teamAcks.push('Contributors');
+		if (Developers.includes(id)) teamAcks.push('Developers');
+		// rest of your if statements go here
+
+		return teamAcks;
+	
 
 	/**
 	 * Function for recieving slash command.
@@ -100,13 +103,15 @@ class UserInfo extends Command {
 	 * @param {args} args The options provided in the command, if any
 	 * @readonly
 	*/
-	reply(bot, interaction, channel, userID) {
-		const member = channel.guild.members.cache.get(userID);
+	reply(bot, interaction, channel) {
+
+
 		const embed = this.createEmbed(bot, channel.guild, member);
-		
+
 		// send embed
 		return interaction.reply({ embeds: [embed] });
 	}
+
 
 	/**
 	 * Function for creating embed of user information
@@ -118,7 +123,6 @@ class UserInfo extends Command {
 	createEmbed(bot, guild, member) {
 		const status = (member.presence?.activities.length >= 1) ? `${member.presence.activities[0].name} - ${(member.presence.activities[0].type == 'CUSTOM_STATUS') ? member.presence.activities[0].state : member.presence.activities[0].details}` : 'None';
 		return new Embed(bot, guild)
-			
 			.setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL() })
 			.setColor(bot.config.embedColor)
 			.setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 512 }))
@@ -129,7 +133,7 @@ class UserInfo extends Command {
 				{ name: guild.translate('guild/user-info:CREATE'), value: moment(member.user.createdAt).format('lll'), inline: true },
 				{ name: guild.translate('guild/user-info:STATUS'), value: `\`${status}\``, inline: true },
 				{ name: guild.translate('guild/user-info:ROLE'), value: `${member.roles.highest}`, inline: true },
-				{ name: if (getAcks(id).length > 0) ("Staff Acknowledgements", getAcks(id), inline: true },
+				{ name: ('Staff Team'), value: acknowledgment.length > 0 ? acknowledgement.join(", ") : "None", inline: true },
 				{ name: guild.translate('guild/user-info:JOIN'), value: moment(member.joinedAt).format('lll'), inline: true },
 				{ name: guild.translate('guild/user-info:NICK'), value: member.nickname != null ? member.nickname : guild.translate('misc:NONE'), inline: true },
 				{ name: guild.translate('guild/user-info:ROLES'), value: member.roles.cache.sort((a, b) => b.rawPosition - a.rawPosition).reduce((a, b) => `${a}, ${b}`) },
@@ -138,3 +142,4 @@ class UserInfo extends Command {
 }
 
 module.exports = UserInfo;
+
